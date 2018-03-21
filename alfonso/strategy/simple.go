3f294306f25dc33e51beb154c65ca5a6d65a383c
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-03-20 18:12:06 -0700

    14 replace simple.go with a composition of two instances of SellSideStrategy

--- alfonso/strategy/simple.go
+++ alfonso/strategy/simple.go
@@ -1,22 +1,11 @@
 package strategy
 
 import (
-	"math"
-
-	"github.com/lightyeario/kelp/alfonso/priceFeed"
 	"github.com/lightyeario/kelp/alfonso/strategy/level"
+	"github.com/lightyeario/kelp/alfonso/strategy/sideStrategy"
 	"github.com/lightyeario/kelp/support"
-	"github.com/stellar/go/build"
 	"github.com/stellar/go/clients/horizon"
-	"github.com/stellar/go/support/log"
 )
-
-/*
-Prices are (amount of B)/(amount of A)
-1BTC/5000xlm=price
-
-Amounts are in A
-*/
 
 // SimpleConfig contains the configuration params for this strategy
 type SimpleConfig struct {
@@ -30,194 +19,49 @@ type SimpleConfig struct {
 	LEVELS           []level.Level `valid:"-"`
 }
 
-// SimpleStrategy is a simple market maker strategy that puts buy and sell orders
-// on each side of a "centered" price based on a configuration file
-type SimpleStrategy struct {
-	txButler *kelp.TxButler
-	assetA   *horizon.Asset
-	assetB   *horizon.Asset
-	config   *SimpleConfig
-	pf       priceFeed.FeedPair
-
-	// uninitialized
-	centerPrice           float64
-	lastPlacedCenterPrice *float64
-	maxAssetA             float64
-	maxAssetB             float64
-}
-
-// ensure that SimpleStrategy implements Strategy
-var _ Strategy = &SimpleStrategy{}
-
 // MakeSimpleStrategy is a factory method
-func MakeSimpleStrategy(txButler *kelp.TxButler, assetA *horizon.Asset, assetB *horizon.Asset, config *SimpleConfig) Strategy {
-	return &SimpleStrategy{
-		txButler: txButler,
-		assetA:   assetA,
-		assetB:   assetB,
-		config:   config,
-		pf: *priceFeed.MakeFeedPair(
-			config.DATA_TYPE_A,
-			config.DATA_FEED_A_URL,
-			config.DATA_TYPE_B,
-			config.DATA_FEED_B_URL,
-		),
-	}
-}
+func MakeSimpleStrategy(
+	txButler *kelp.TxButler,
+	assetBase *horizon.Asset,
+	assetQuote *horizon.Asset,
+	config *SimpleConfig,
+) Strategy {
+	sellSideStrategy := sideStrategy.MakeSellSideStrategy(
+		txButler,
+		assetBase,
+		assetQuote,
+		&sideStrategy.SellSideConfig{
+			DATA_TYPE_A:      config.DATA_TYPE_A,
+			DATA_FEED_A_URL:  config.DATA_FEED_A_URL,
+			DATA_TYPE_B:      config.DATA_TYPE_B,
+			DATA_FEED_B_URL:  config.DATA_FEED_B_URL,
+			PRICE_TOLERANCE:  config.PRICE_TOLERANCE,
+			AMOUNT_TOLERANCE: config.AMOUNT_TOLERANCE,
+			AMOUNT_OF_A_BASE: config.AMOUNT_OF_A_BASE,
+			LEVELS:           config.LEVELS,
+		},
+	)
+	// switch sides of base/quote here for buy side
+	buySideStrategy := sideStrategy.MakeSellSideStrategy(
+		txButler,
+		assetQuote,
+		assetBase,
+		&sideStrategy.SellSideConfig{
+			DATA_TYPE_A:      config.DATA_TYPE_B,
+			DATA_FEED_A_URL:  config.DATA_FEED_B_URL,
+			DATA_TYPE_B:      config.DATA_TYPE_A,
+			DATA_FEED_B_URL:  config.DATA_FEED_A_URL,
+			PRICE_TOLERANCE:  config.PRICE_TOLERANCE,
+			AMOUNT_TOLERANCE: config.AMOUNT_TOLERANCE,
+			AMOUNT_OF_A_BASE: config.AMOUNT_OF_A_BASE,
+			LEVELS:           config.LEVELS,
+		},
+	)
 
-// PruneExistingOffers deletes any extra offers
-func (s SimpleStrategy) PruneExistingOffers(buyingAOffers []horizon.Offer, sellingAOffers []horizon.Offer) ([]horizon.Offer, []horizon.Offer) {
-	return s.pruneExistingOffersHelper(buyingAOffers), s.pruneExistingOffersHelper(sellingAOffers)
-}
-
-func (s SimpleStrategy) pruneExistingOffersHelper(offers []horizon.Offer) []horizon.Offer {
-	for i := len(s.config.LEVELS); i < len(offers); i++ {
-		s.txButler.DeleteOffer(offers[i])
-	}
-	if len(offers) > len(s.config.LEVELS) {
-		return offers[:len(s.config.LEVELS)]
-	}
-	return offers
-}
-
-// PreUpdate changes the strategy's state in prepration for the update
-func (s *SimpleStrategy) PreUpdate(
-	maxAssetA float64,
-	maxAssetB float64,
-	buyingAOffers []horizon.Offer,
-	sellingAOffers []horizon.Offer,
-) error {
-	s.maxAssetA = maxAssetA
-	s.maxAssetB = maxAssetB
-
-	// load lastPlacedCenterPrice
-	if s.lastPlacedCenterPrice == nil && len(buyingAOffers) > 0 && len(sellingAOffers) > 0 {
-		last := (1/kelp.PriceAsFloat(buyingAOffers[0].Price) + kelp.PriceAsFloat(sellingAOffers[0].Price)) / 2
-		s.lastPlacedCenterPrice = &last
-		log.Info("loaded lastPlacedCenterPrice: ", *s.lastPlacedCenterPrice)
-	}
-
-	var e error
-	s.centerPrice, e = s.pf.GetCenterPrice()
-	if e != nil {
-		log.Error("Center price couldn't be loaded! ", e)
-	} else {
-		log.Info("Center price: ", s.centerPrice, "        v0.2")
-	}
-	return e
-}
-
-// UpdateWithOps builds the operations we want performed on the account
-func (s SimpleStrategy) UpdateWithOps(buyingAOffers []horizon.Offer, sellingAOffers []horizon.Offer) ([]build.TransactionMutator, error) {
-	sellOps := s.updateLevels(bindOffersToUpdateLevel(s.updateSellLevel, sellingAOffers))
-	buyOps := s.updateLevels(bindOffersToUpdateLevel(s.updateBuyLevel, buyingAOffers))
-
-	ops := []build.TransactionMutator{}
-	if s.centerPrice < *s.lastPlacedCenterPrice {
-		ops = append(ops, buyOps...)
-		ops = append(ops, sellOps...)
-	} else {
-		ops = append(ops, sellOps...)
-		ops = append(ops, buyOps...)
-	}
-
-	return ops, nil
-}
-
-// PostUpdate changes the strategy's state after the update has taken place
-func (s *SimpleStrategy) PostUpdate() error {
-	*s.lastPlacedCenterPrice = s.centerPrice
-	return nil
-}
-
-func bindOffersToUpdateLevel(
-	updateLevel func(offers []horizon.Offer, index int) *build.ManageOfferBuilder,
-	offers []horizon.Offer,
-) func(index int) *build.ManageOfferBuilder {
-	return func(index int) *build.ManageOfferBuilder {
-		return updateLevel(offers, index)
-	}
-}
-
-func (s *SimpleStrategy) updateLevels(
-	updateLevel func(index int) *build.ManageOfferBuilder,
-) []build.TransactionMutator {
-	ops := []build.TransactionMutator{}
-	for i := len(s.config.LEVELS) - 1; i >= 0; i-- {
-		op := updateLevel(i)
-		if op != nil {
-			ops = append(ops, op)
-		}
-	}
-	return ops
-}
-
-// Buying A
-func (s *SimpleStrategy) updateBuyLevel(buyingAOffers []horizon.Offer, index int) *build.ManageOfferBuilder {
-	spread := s.centerPrice * s.config.LEVELS[index].SPREAD
-	targetPrice := s.centerPrice - spread
-
-	targetAmount := s.config.LEVELS[index].AMOUNT * s.config.AMOUNT_OF_A_BASE
-	targetAmount = math.Min(targetAmount, s.maxAssetB/targetPrice)
-
-	if len(buyingAOffers) <= index {
-		// no existing offer at this index
-		log.Info("create buy: target:", targetPrice, " ta:", targetAmount)
-
-		return s.txButler.CreateBuyOffer(*s.assetA, *s.assetB, targetPrice, targetAmount)
-	}
-
-	highestPrice := targetPrice + targetPrice*s.config.PRICE_TOLERANCE
-	lowestPrice := targetPrice - targetPrice*s.config.PRICE_TOLERANCE
-	//check if existing offer needs to be modified
-	curPrice := kelp.GetInvertedPrice(buyingAOffers[index])
-	curAmount := kelp.AmountStringAsFloat(buyingAOffers[index].Amount) * kelp.PriceAsFloat(buyingAOffers[index].Price)
-
-	minAmount := targetAmount - targetAmount*s.config.AMOUNT_TOLERANCE
-	maxAmount := targetAmount + targetAmount*s.config.AMOUNT_TOLERANCE
-
-	if (curPrice > highestPrice) ||
-		(curPrice < lowestPrice) ||
-		(curAmount < minAmount) ||
-		(curAmount > maxAmount) {
-		// existing offer not within tolerances
-		log.Info("mod buy:", curPrice, " a:", curAmount)
-		return s.txButler.ModifyBuyOffer(buyingAOffers[index], targetPrice, targetAmount)
-	}
-	return nil
-}
-
-// Selling A
-func (s *SimpleStrategy) updateSellLevel(sellingAOffers []horizon.Offer, index int) *build.ManageOfferBuilder {
-	spread := s.centerPrice * s.config.LEVELS[index].SPREAD
-	targetPrice := s.centerPrice + spread
-	targetAmount := s.config.LEVELS[index].AMOUNT * s.config.AMOUNT_OF_A_BASE
-	targetAmount = math.Min(targetAmount, s.maxAssetA)
-
-	if len(sellingAOffers) <= index {
-		// no existing offer at this index
-		log.Info("create sell: target:", targetPrice, " ta:", targetAmount)
-		return s.txButler.CreateSellOffer(*s.assetA, *s.assetB, targetPrice, targetAmount)
-	}
-
-	highestPrice := targetPrice + targetPrice*s.config.PRICE_TOLERANCE
-	lowestPrice := targetPrice - targetPrice*s.config.PRICE_TOLERANCE
-
-	//check if existing offer needs to be modified
-	curPrice := kelp.GetPrice(sellingAOffers[index])
-	curAmount := kelp.AmountStringAsFloat(sellingAOffers[index].Amount)
-
-	minAmount := targetAmount - targetAmount*s.config.AMOUNT_TOLERANCE
-	maxAmount := targetAmount + targetAmount*s.config.AMOUNT_TOLERANCE
-
-	if (curPrice > highestPrice) ||
-		(curPrice < lowestPrice) ||
-		(curAmount < minAmount) ||
-		(curAmount > maxAmount) {
-		// existing offer not within tolerances
-		log.Info("mod sell curPrice", curPrice, " hp:", highestPrice, " lp:", lowestPrice, " ca:", curAmount, " minA:", minAmount, " maxA:", maxAmount)
-
-		return s.txButler.ModifySellOffer(sellingAOffers[index], targetPrice, targetAmount)
-	}
-	return nil
+	return MakeComposeStrategy(
+		assetBase,
+		assetQuote,
+		buySideStrategy,
+		sellSideStrategy,
+	)
 }
