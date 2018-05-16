d039fb09cf3bcfd38d4d97ac46e972ff61f36890
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-05-16 15:47:31 -0700

    extract parsing logic for Kraken to parser.go

---
+++ support/exchange/kraken/parser.go
@@ -0,0 +1,96 @@
+package kraken
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/lightyeario/kelp/support/exchange/api/number"
+)
+
+const numberPrecision = 10
+const prefixFieldNotFound = "could not find field in map of PrepareDeposit"
+
+func checkKeyPresent(m map[string]interface{}, key string) (interface{}, error) {
+	v, ok := m[key]
+	if !ok {
+		return nil, fmt.Errorf("%s: %s", prefixFieldNotFound, key)
+	}
+
+	return v, nil
+}
+
+func makeParseError(field string, dataType string, methodAPI string, value interface{}) error {
+	return fmt.Errorf("could not parse the field '%s' as a %s in the response from %s: value=%v, type=%s", field, dataType, methodAPI, value, reflect.TypeOf(value))
+}
+
+func parseString(m map[string]interface{}, key string, methodAPI string) (string, error) {
+	v, e := checkKeyPresent(m, key)
+	if e != nil {
+		return "", e
+	}
+
+	s, ok := v.(string)
+	if !ok {
+		return "", makeParseError(key, "string", methodAPI, v)
+	}
+
+	return s, nil
+}
+
+func parseBool(m map[string]interface{}, key string, methodAPI string) (bool, error) {
+	v, e := checkKeyPresent(m, key)
+	if e != nil {
+		return false, e
+	}
+
+	b, ok := v.(bool)
+	if !ok {
+		return false, makeParseError(key, "bool", methodAPI, v)
+	}
+
+	return b, nil
+}
+
+func parseNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
+	v, e := checkKeyPresent(m, key)
+	if e != nil {
+		return nil, e
+	}
+
+	switch v.(type) {
+	case string:
+		return parseStringAsNumber(m, key, methodAPI)
+	case float64:
+		return parseFloatAsNumber(m, key, methodAPI)
+	default:
+		return nil, makeParseError(key, "number", methodAPI, v)
+	}
+}
+
+func parseStringAsNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
+	s, e := parseString(m, key, methodAPI)
+	if e != nil {
+		return nil, e
+	}
+
+	n, e := number.FromString(s, numberPrecision)
+	if e != nil {
+		return nil, fmt.Errorf("unable to convert the string field '%s' to a number in the response from %s: value=%v, error=%s", key, methodAPI, s, e)
+	}
+
+	return n, nil
+}
+
+func parseFloatAsNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
+	v, e := checkKeyPresent(m, key)
+	if e != nil {
+		return nil, e
+	}
+
+	f, ok := v.(float64)
+	if !ok {
+		return nil, makeParseError(key, "float", methodAPI, v)
+	}
+
+	return number.FromFloat(f, numberPrecision), nil
+}
--- support/exchange/kraken/prepareDeposit.go
+++ support/exchange/kraken/prepareDeposit.go
@@ -10,8 +10,6 @@ import (
 	"github.com/lightyeario/kelp/support/treasury/api"
 )
 
-const numberPrecision = 10
-
 // PrepareDeposit impl.
 func (k krakenExchange) PrepareDeposit(asset assets.Asset, amount *number.Number) (*treasury.PrepareDepositResult, error) {
 	krakenAsset, e := k.assetConverter.ToString(asset)
@@ -217,91 +215,4 @@ func parseDepositMethods(m map[string]interface{}) (*depositMethod, error) {
 		fee:        fee,
 		genAddress: genAddress,
 	}, nil
-}
-
-const prefixFieldNotFound = "could not find field in map of PrepareDeposit"
-
-func checkKeyPresent(m map[string]interface{}, key string) (interface{}, error) {
-	v, ok := m[key]
-	if !ok {
-		return nil, fmt.Errorf("%s: %s", prefixFieldNotFound, key)
-	}
-
-	return v, nil
-}
-
-func makeParseError(field string, dataType string, methodAPI string, value interface{}) error {
-	return fmt.Errorf("could not parse the field '%s' as a %s in the response from %s: value=%v, type=%s", field, dataType, methodAPI, value, reflect.TypeOf(value))
-}
-
-func parseString(m map[string]interface{}, key string, methodAPI string) (string, error) {
-	v, e := checkKeyPresent(m, key)
-	if e != nil {
-		return "", e
-	}
-
-	s, ok := v.(string)
-	if !ok {
-		return "", makeParseError(key, "string", methodAPI, v)
-	}
-
-	return s, nil
-}
-
-func parseBool(m map[string]interface{}, key string, methodAPI string) (bool, error) {
-	v, e := checkKeyPresent(m, key)
-	if e != nil {
-		return false, e
-	}
-
-	b, ok := v.(bool)
-	if !ok {
-		return false, makeParseError(key, "bool", methodAPI, v)
-	}
-
-	return b, nil
-}
-
-func parseNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
-	v, e := checkKeyPresent(m, key)
-	if e != nil {
-		return nil, e
-	}
-
-	switch v.(type) {
-	case string:
-		return parseStringAsNumber(m, key, methodAPI)
-	case float64:
-		return parseFloatAsNumber(m, key, methodAPI)
-	default:
-		return nil, makeParseError(key, "number", methodAPI, v)
-	}
-}
-
-func parseStringAsNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
-	s, e := parseString(m, key, methodAPI)
-	if e != nil {
-		return nil, e
-	}
-
-	n, e := number.FromString(s, numberPrecision)
-	if e != nil {
-		return nil, fmt.Errorf("unable to convert the string field '%s' to a number in the response from %s: value=%v, error=%s", key, methodAPI, s, e)
-	}
-
-	return n, nil
-}
-
-func parseFloatAsNumber(m map[string]interface{}, key string, methodAPI string) (*number.Number, error) {
-	v, e := checkKeyPresent(m, key)
-	if e != nil {
-		return nil, e
-	}
-
-	f, ok := v.(float64)
-	if !ok {
-		return nil, makeParseError(key, "float", methodAPI, v)
-	}
-
-	return number.FromFloat(f, numberPrecision), nil
 }
