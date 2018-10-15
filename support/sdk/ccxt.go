a0814bd7cc50476f1bb726a4babfdef961442d99
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-10-15 16:46:24 -0700

    ccxt: reduce cyclomatic complexity for init() method

--- support/sdk/ccxt.go
+++ support/sdk/ccxt.go
@@ -3,6 +3,7 @@ package sdk
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
@@ -69,23 +70,11 @@ func (c *Ccxt) init() error {
 	// make a new instance if needed
 	if len(instanceList) == 0 {
 		instanceName := c.exchangeName + "1"
-		var data []byte
-		data, e = json.Marshal(&struct {
-			ID string `json:"id"`
-		}{
-			ID: instanceName,
-		})
+		e = c.newInstance(instanceName)
 		if e != nil {
-			return fmt.Errorf("error marshaling instanceName '%s' as ID for exchange '%s': %s", instanceName, c.exchangeName, e)
+			return fmt.Errorf("error creating new instance '%s' for exchange '%s': %s", instanceName, c.exchangeName, e)
 		}
-		var newInstance map[string]interface{}
-		e = utils.JSONRequest(c.httpClient, "POST", c.ccxtBaseURL+pathExchanges+"/"+c.exchangeName, string(data), map[string]string{}, &newInstance)
-		if e != nil {
-			return fmt.Errorf("error creating new exchange instance for exchange '%s': %s", c.exchangeName, e)
-		}
-		if _, ok := newInstance["urls"]; !ok {
-			return fmt.Errorf("unable to create a new instance of exchange '%s' with instanceName: %s", c.exchangeName, instanceName)
-		}
+		log.Printf("created new instance '%s' for exchange '%s'\n", instanceName, c.exchangeName)
 		c.instanceName = instanceName
 	} else {
 		c.instanceName = instanceList[0]
@@ -101,42 +90,71 @@ func (c *Ccxt) init() error {
 	return nil
 }
 
-// FetchTicker calls the /fetchTicker endpoint on CCXT, trading pair is the CCXT version of the trading pair
-func (c *Ccxt) FetchTicker(tradingPair string) (map[string]interface{}, error) {
+func (c *Ccxt) newInstance(instanceName string) error {
+	data, e := json.Marshal(&struct {
+		ID string `json:"id"`
+	}{
+		ID: instanceName,
+	})
+	if e != nil {
+		return fmt.Errorf("error marshaling instanceName '%s' as ID for exchange '%s': %s", instanceName, c.exchangeName, e)
+	}
+
+	var newInstance map[string]interface{}
+	e = utils.JSONRequest(c.httpClient, "POST", c.ccxtBaseURL+pathExchanges+"/"+c.exchangeName, string(data), map[string]string{}, &newInstance)
+	if e != nil {
+		return fmt.Errorf("error in web request when creating new exchange instance for exchange '%s': %s", c.exchangeName, e)
+	}
+
+	if _, ok := newInstance["urls"]; !ok {
+		return fmt.Errorf("check for new instance of exchange '%s' failed for instanceName: %s", c.exchangeName, instanceName)
+	}
+	return nil
+}
+
+// symbolExists returns an error if the symbol does not exist
+func (c *Ccxt) symbolExists(tradingPair string) error {
 	// get list of symbols available on exchange
 	url := c.ccxtBaseURL + pathExchanges + "/" + c.exchangeName + "/" + c.instanceName
 	// decode generic data (see "https://blog.golang.org/json-and-go#TOC_4.")
 	var exchangeOutput interface{}
 	e := utils.JSONRequest(c.httpClient, "GET", url, "", map[string]string{}, &exchangeOutput)
 	if e != nil {
-		return nil, fmt.Errorf("error fetching details of exchange instance (exchange=%s, instanceName=%s): %s", c.exchangeName, c.instanceName, e)
+		return fmt.Errorf("error fetching details of exchange instance (exchange=%s, instanceName=%s): %s", c.exchangeName, c.instanceName, e)
 	}
 
 	exchangeMap := exchangeOutput.(map[string]interface{})
 	if _, ok := exchangeMap["symbols"]; !ok {
-		return nil, fmt.Errorf("'symbols' field not in result of exchange details (exchange=%s, instanceName=%s)", c.exchangeName, c.instanceName)
+		return fmt.Errorf("'symbols' field not in result of exchange details (exchange=%s, instanceName=%s)", c.exchangeName, c.instanceName)
 	}
+
 	symbolsList := exchangeMap["symbols"].([]interface{})
-	symbolExists := false
 	for _, p := range symbolsList {
 		symbol := p.(string)
 		if tradingPair == symbol {
-			symbolExists = true
-			break
+			// exists
+			return nil
 		}
 	}
-	if !symbolExists {
-		return nil, fmt.Errorf("trading pair '%s' does not exist in the list of %d symbols on exchange '%s'", tradingPair, len(symbolsList), c.exchangeName)
+	return fmt.Errorf("trading pair '%s' does not exist in the list of %d symbols on exchange '%s'", tradingPair, len(symbolsList), c.exchangeName)
+}
+
+// FetchTicker calls the /fetchTicker endpoint on CCXT, trading pair is the CCXT version of the trading pair
+func (c *Ccxt) FetchTicker(tradingPair string) (map[string]interface{}, error) {
+	e := c.symbolExists(tradingPair)
+	if e != nil {
+		return nil, fmt.Errorf("symbol does not exist: %s", e)
 	}
 
-	// fetch ticker for symbol
-	url = c.ccxtBaseURL + pathExchanges + "/" + c.exchangeName + "/" + c.instanceName + "/fetchTicker"
-	// decode generic data (see "https://blog.golang.org/json-and-go#TOC_4.")
-	var data []byte
-	data, e = json.Marshal(&[]string{tradingPair})
+	// marshal input data
+	data, e := json.Marshal(&[]string{tradingPair})
 	if e != nil {
 		return nil, fmt.Errorf("error marshaling tradingPair '%s' as an array for exchange '%s': %s", tradingPair, c.exchangeName, e)
 	}
+
+	// fetch ticker for symbol
+	url := c.ccxtBaseURL + pathExchanges + "/" + c.exchangeName + "/" + c.instanceName + "/fetchTicker"
+	// decode generic data (see "https://blog.golang.org/json-and-go#TOC_4.")
 	var tickerOutput interface{}
 	e = utils.JSONRequest(c.httpClient, "POST", url, string(data), map[string]string{}, &tickerOutput)
 	if e != nil {
