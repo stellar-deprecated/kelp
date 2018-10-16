18c8b0255defd0d2c9fc265de782e42ca6865826
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-10-15 18:43:18 -0700

    ccxt: implement FetchOrderBook in SDK with test

--- support/sdk/ccxt_test.go
+++ support/sdk/ccxt_test.go
@@ -67,3 +67,97 @@ func TestFetchTickersWithMissingSymbol(t *testing.T) {
 	}
 	// success
 }
+
+func TestFetchOrderBook(t *testing.T) {
+	limit5 := 5
+	limit2 := 2
+	for _, k := range []orderbookTest{
+		{
+			exchangeName: "poloniex",
+			tradingPair:  "BTC/USDT",
+			limit:        nil,
+			expectError:  false,
+		}, {
+			exchangeName: "poloniex",
+			tradingPair:  "BTC/USDT",
+			limit:        &limit5,
+			expectError:  false,
+		}, {
+			exchangeName: "poloniex",
+			tradingPair:  "BTC/USDT",
+			limit:        &limit2,
+			expectError:  false,
+		}, {
+			exchangeName: "binance",
+			tradingPair:  "BTC/USDT",
+			limit:        nil,
+			expectError:  false,
+		}, {
+			exchangeName: "binance",
+			tradingPair:  "BTC/USDT",
+			limit:        &limit5,
+			expectError:  false,
+		}, {
+			exchangeName: "binance",
+			tradingPair:  "BTC/USDT",
+			limit:        &limit2,
+			expectError:  true,
+		}, {
+			exchangeName: "poloniex",
+			tradingPair:  "XLM/USDT",
+			limit:        &limit2,
+			expectError:  false,
+		},
+	} {
+		name := fmt.Sprintf("%s-nil", k.exchangeName)
+		if k.limit != nil {
+			name = fmt.Sprintf("%s-%v", k.exchangeName, *k.limit)
+		}
+
+		t.Run(name, func(t *testing.T) {
+			runTestFetchOrderBook(k, t)
+		})
+	}
+}
+
+type orderbookTest struct {
+	exchangeName string
+	tradingPair  string
+	limit        *int
+	expectError  bool
+}
+
+func runTestFetchOrderBook(k orderbookTest, t *testing.T) {
+	c, e := MakeInitializedCcxtExchange("http://localhost:3000", k.exchangeName)
+	if e != nil {
+		assert.Fail(t, fmt.Sprintf("error when making ccxt exchange: %s", e))
+		return
+	}
+
+	m, e := c.FetchOrderBook(k.tradingPair, k.limit)
+	if e != nil && !k.expectError {
+		assert.Fail(t, fmt.Sprintf("error when fetching tickers: %s", e))
+		return
+	} else if e != nil {
+		// success
+		return
+	} else if e == nil && k.expectError {
+		assert.Fail(t, fmt.Sprintf("expected error when fetching tickers but nothing thrown"))
+		return
+	}
+	// else run checks below
+
+	validateOrders := func(key string) {
+		orders, ok := m[key]
+		assert.True(t, ok)
+		if k.limit != nil {
+			assert.Equal(t, len(orders), *k.limit)
+		}
+		for _, o := range orders {
+			assert.True(t, o.Price > 0)
+			assert.True(t, o.Amount > 0)
+		}
+	}
+	validateOrders("asks")
+	validateOrders("bids")
+}
