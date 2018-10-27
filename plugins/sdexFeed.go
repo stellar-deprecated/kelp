b1297b5743df19d1be2405b20388c1f071036504
Reid McCamish <43561569+Reidmcc@users.noreply.github.com>
2018-10-27 14:21:07 -0500

    Add price pull func from SDEX

---
+++ plugins/sdexFeed.go
@@ -0,0 +1,22 @@
+package plugins
+
+import (
+	"github.com/lightyeario/kelp/support/utils"
+	"github.com/stellar/go/clients/horizon"
+)
+
+func GetSDEXPrice(sdex *SDEX, assetBase *horizon.Asset, assetQuote *horizon.Asset) (float64, error) {
+	orderBook, e := utils.GetOrderBook(sdex.API, assetBase, assetQuote)
+	if e != nil {
+		return 0, e
+	}
+
+	bids := orderBook.Bids
+	topBidPrice := utils.PriceAsFloat(bids[0].Price)
+
+	asks := orderBook.Asks
+	lowAskPrice := utils.PriceAsFloat(asks[0].Price)
+
+	centerPrice := (topBidPrice + lowAskPrice) / 2
+	return centerPrice, nil
+}
