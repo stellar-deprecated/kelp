578b9d13a974f471c094b0b3dbaf2a27b3f3e41b
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-12-03 12:26:42 -0800

    add math functions to model.Number

--- model/number_test.go
+++ model/number_test.go
@@ -7,6 +7,126 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNumberFromFloat(t *testing.T) {
+	testCases := []struct {
+		f          float64
+		precision  int8
+		wantString string
+		wantFloat  float64
+	}{
+		{
+			f:          1.1,
+			precision:  1,
+			wantString: "1.1",
+			wantFloat:  1.1,
+		}, {
+			f:          1.1,
+			precision:  2,
+			wantString: "1.10",
+			wantFloat:  1.10,
+		}, {
+			f:          1.12,
+			precision:  1,
+			wantString: "1.1",
+			wantFloat:  1.1,
+		}, {
+			f:          1.15,
+			precision:  1,
+			wantString: "1.2",
+			wantFloat:  1.2,
+		}, {
+			f:          0.12,
+			precision:  1,
+			wantString: "0.1",
+			wantFloat:  0.1,
+		},
+	}
+
+	for _, kase := range testCases {
+		t.Run(fmt.Sprintf("%f_%d", kase.f, kase.precision), func(t *testing.T) {
+			n := NumberFromFloat(kase.f, kase.precision)
+			if !assert.Equal(t, kase.wantString, n.AsString()) {
+				return
+			}
+			if !assert.Equal(t, kase.wantFloat, n.AsFloat()) {
+				return
+			}
+		})
+	}
+}
+
+func TestMath(t *testing.T) {
+	testCases := []struct {
+		n1           *Number
+		n2           *Number
+		wantAdd      float64
+		wantSubtract float64
+		wantMultiply float64
+		wantDivide   float64
+	}{
+		{
+			n1:           NumberFromFloat(1.1, 1),
+			n2:           NumberFromFloat(2.1, 1),
+			wantAdd:      3.2,
+			wantSubtract: -1.0,
+			wantMultiply: 2.3,
+			wantDivide:   0.5,
+		}, {
+			n1:           NumberFromFloat(1.15, 1),
+			n2:           NumberFromFloat(2.1, 1),
+			wantAdd:      3.3,
+			wantSubtract: -0.9,
+			wantMultiply: 2.5,
+			wantDivide:   0.6,
+		}, {
+			n1:           NumberFromFloat(1.15, 2),
+			n2:           NumberFromFloat(2.1, 1),
+			wantAdd:      3.3,
+			wantSubtract: -1.0,
+			wantMultiply: 2.4,
+			wantDivide:   0.5,
+		}, {
+			n1:           NumberFromFloat(1.15, 2),
+			n2:           NumberFromFloat(2.1, 2),
+			wantAdd:      3.25,
+			wantSubtract: -0.95,
+			wantMultiply: 2.42,
+			wantDivide:   0.55,
+		}, {
+			n1:           NumberFromFloat(1.12, 2),
+			n2:           NumberFromFloat(2.1, 1),
+			wantAdd:      3.2,
+			wantSubtract: -1.0,
+			wantMultiply: 2.4,
+			wantDivide:   0.5,
+		},
+	}
+
+	for i, kase := range testCases {
+		t.Run(fmt.Sprintf("%d__%f_%d__%f_%d", i, kase.n1.AsFloat(), kase.n1.Precision(), kase.n2.AsFloat(), kase.n2.Precision()), func(t *testing.T) {
+			n := kase.n1.Add(*kase.n2)
+			if !assert.Equal(t, kase.wantAdd, n.AsFloat()) {
+				return
+			}
+
+			n = kase.n1.Subtract(*kase.n2)
+			if !assert.Equal(t, kase.wantSubtract, n.AsFloat()) {
+				return
+			}
+
+			n = kase.n1.Multiply(*kase.n2)
+			if !assert.Equal(t, kase.wantMultiply, n.AsFloat()) {
+				return
+			}
+
+			n = kase.n1.Divide(*kase.n2)
+			if !assert.Equal(t, kase.wantDivide, n.AsFloat()) {
+				return
+			}
+		})
+	}
+}
+
 func TestAsRatio(t *testing.T) {
 	testCases := []struct {
 		n     *Number
