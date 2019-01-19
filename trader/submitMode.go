51535dcf739a357d7475521a36929ea0a70b68e6
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2019-01-18 16:23:48 -0800

    B

--- trader/submitMode.go
+++ trader/submitMode.go
@@ -2,6 +2,7 @@ package trader
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/interstellar/kelp/model"
 	"github.com/interstellar/kelp/plugins"
@@ -31,6 +32,18 @@ func ParseSubmitMode(submitMode string) SubmitMode {
 	return SubmitModeBoth
 }
 
+func (s *SubmitMode) String() string {
+	if *s == SubmitModeMakerOnly {
+		return "maker_only"
+	}
+
+	if *s == SubmitModeTakerOnly {
+		return "taker_only"
+	}
+
+	return "both"
+}
+
 // submitFilter allows you to filter out operations before submitting to the network
 type submitFilter interface {
 	apply(ops []build.TransactionMutator) ([]build.TransactionMutator, error)
@@ -38,11 +51,19 @@ type submitFilter interface {
 
 // makeSubmitFilter makes a submit filter based on the passed in submitMode
 func makeSubmitFilter(submitMode SubmitMode, sdex *plugins.SDEX, tradingPair *model.TradingPair) submitFilter {
-	if submitMode == SubmitModeMakerOnly || submitMode == SubmitModeTakerOnly {
+	if submitMode == SubmitModeMakerOnly {
 		return &sdexFilter{
 			tradingPair: tradingPair,
 			sdex:        sdex,
 			submitMode:  submitMode,
+			filter:      filterMakerMode,
+		}
+	} else if submitMode == SubmitModeTakerOnly {
+		return &sdexFilter{
+			tradingPair: tradingPair,
+			sdex:        sdex,
+			submitMode:  submitMode,
+			filter:      filterTakerMode,
 		}
 	}
 	return nil
@@ -52,6 +73,7 @@ type sdexFilter struct {
 	tradingPair *model.TradingPair
 	sdex        *plugins.SDEX
 	submitMode  SubmitMode
+	filter      func(ops []build.TransactionMutator, ob *model.OrderBook) ([]build.TransactionMutator, error)
 }
 
 var _ submitFilter = &sdexFilter{}
@@ -63,30 +85,49 @@ func (f *sdexFilter) apply(ops []build.TransactionMutator) ([]build.TransactionM
 	// if e != nil {
 	// 	return nil, fmt.Errorf("could not fetch SDEX orderbook: %s", e)
 	// }
+
 	var e error
+	ops, e = f.filter(ops, ob)
+	if e != nil {
+		return nil, fmt.Errorf("could not apply filter (submitMode=%s): %s", f.submitMode, e)
+	}
+	return ops, nil
+}
+
+func filterMakerMode(ops []build.TransactionMutator, ob *model.OrderBook) ([]build.TransactionMutator, error) {
+	numDropped := 0
+	filteredOps := []build.TransactionMutator{}
+	for _, op := range ops {
+		switch o := op.(type) {
+		case *build.ManageOfferBuilder:
+			if shouldKeepOfferMakerMode(ob, o) {
+				filteredOps = append(filteredOps, o)
+			} else {
+				numDropped++
+			}
+		case build.ManageOfferBuilder:
+			if shouldKeepOfferMakerMode(ob, &o) {
+				filteredOps = append(filteredOps, o)
+			} else {
+				numDropped++
+			}
+		default:
+			filteredOps = append(filteredOps, o)
+		}
+	}
+	log.Printf("dropped %d operations in the maker filter", numDropped)
+	return nil, nil
+}
+
+func shouldKeepOfferMakerMode(ob *model.OrderBook, o *build.ManageOfferBuilder) bool {
 
 	// TODO find intersection of orderbook and ops
 	/*
 		1. get top bid and top ask in OB
 		2. for each op remove or keep op if it is before/after top bid/ask depending on the mode we're in
 	*/
+	float64(o.MO.Price.N) / float64(o.MO.Price.D)
 
-	if f.submitMode == SubmitModeMakerOnly {
-		ops, e = filterMakerMode(ops, ob)
-		if e != nil {
-			return nil, fmt.Errorf("could not apply maker mode filter: %s", e)
-		}
-	} else {
-		ops, e = filterTakerMode(ops, ob)
-		if e != nil {
-			return nil, fmt.Errorf("could not apply taker mode filter: %s", e)
-		}
-	}
-	return ops, nil
-}
-
-func filterMakerMode(ops []build.TransactionMutator, ob *model.OrderBook) ([]build.TransactionMutator, error) {
-	return nil, nil
 }
 
 func filterTakerMode(ops []build.TransactionMutator, ob *model.OrderBook) ([]build.TransactionMutator, error) {
