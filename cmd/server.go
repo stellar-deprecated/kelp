38b098c670d4599c5740f3adf356068583458a27
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2019-04-16 14:33:00 -0700

    move Filserver() to gui package
    fixes issue of "build constraints exclude all Go files" since we now have something in the gui package that is always compiled

--- cmd/server.go
+++ cmd/server.go
@@ -50,7 +50,7 @@ func init() {
 		r.Use(middleware.Recoverer)
 		r.Use(middleware.Timeout(60 * time.Second))
 		// gui.FS is automatically compiled based on whether this is a local or deployment build
-		fileServer(r, "/", gui.FS)
+		gui.FileServer(r, "/", gui.FS)
 
 		portString := fmt.Sprintf(":%d", *options.port)
 		log.Printf("Serving on HTTP port: %d\n", *options.port)
@@ -112,24 +112,4 @@ func runCommandStreamOutput(command *exec.Cmd) error {
 		return fmt.Errorf("could not execute command: %s", e)
 	}
 	return nil
-}
-
-// fileServer sets up a http.FileServer handler to serve static files from a http.FileSystem
-// example taken from here: https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
-func fileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit URL parameters.")
-	}
-
-	fs := http.StripPrefix(path, http.FileServer(root))
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fs.ServeHTTP(w, r)
-	}))
 }
---
+++ gui/fileserver.go
@@ -0,0 +1,28 @@
+package gui
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi"
+)
+
+// FileServer sets up a http.FileServer handler to serve static files from a http.FileSystem
+// example taken from here: https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
+}
