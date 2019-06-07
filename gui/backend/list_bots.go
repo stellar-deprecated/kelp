1a2495603b6a6458843c06b8035cca5eaf614bf2
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2019-06-07 12:11:07 -0700

    1 - list bots endpoint

---
+++ gui/backend/list_bots.go
@@ -0,0 +1,39 @@
+package backend
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/stellar/kelp/gui/model"
+)
+
+func (s *APIServer) listBots(w http.ResponseWriter, r *http.Request) {
+	log.Printf("listing bots\n")
+	resultBytes, e := s.kos.Blocking("ls", fmt.Sprintf("ls %s | sort", s.configsDir))
+	if e != nil {
+		s.writeError(w, fmt.Sprintf("error when listing bots: %s\n", e))
+		return
+	}
+	configFiles := string(resultBytes)
+	files := strings.Split(configFiles, "\n")
+
+	bots := []model.Bot{}
+	// run till one less than length of files because the last name will end in a newline
+	for i := 0; i < len(files)-1; i += 2 {
+		bot := model.FromFilenames(files[i+1], files[i])
+		bots = append(bots, *bot)
+	}
+	log.Printf("bots available: %v", bots)
+
+	// serialize and return
+	botsJson, e := json.Marshal(bots)
+	if e != nil {
+		s.writeError(w, fmt.Sprintf("unable to serialize bots: %s\n", e))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(botsJson)
+}
--- gui/backend/routes.go
+++ gui/backend/routes.go
@@ -10,6 +10,7 @@ import (
 func SetRoutes(r *chi.Mux, s *APIServer) {
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/version", http.HandlerFunc(s.version))
+		r.Get("/listBots", http.HandlerFunc(s.listBots))
 		r.Get("/autogenerate", http.HandlerFunc(s.autogenerateBot))
 		r.Post("/start", http.HandlerFunc(s.startBot))
 		r.Post("/stop", http.HandlerFunc(s.stopBot))
