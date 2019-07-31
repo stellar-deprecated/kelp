8addb89090ec9d5445deed503c3c71c42943feba
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2019-07-30 23:24:42 -0700

    10 - add all fiat currencies available via currencylayer

--- gui/backend/options_metadata.go
+++ gui/backend/options_metadata.go
@@ -274,8 +274,10 @@ func (dob *dropdownOptionsBuilder) coinmarketcap(tickerCode string, currencyName
 	return dob._leaf(fmt.Sprintf("https://api.coinmarketcap.com/v1/ticker/%s/", tickerCode), currencyName)
 }
 
-func (dob *dropdownOptionsBuilder) currencylayer(tickerCode string) *dropdownOptionsBuilder {
-	return dob._leaf(fmt.Sprintf("http://apilayer.net/api/live?access_key=8db4ba3aa504c601dd513777193f4f2b&currencies=%s", tickerCode), tickerCode)
+func (dob *dropdownOptionsBuilder) currencylayer(tickerCode string, name string) *dropdownOptionsBuilder {
+	value := fmt.Sprintf("http://apilayer.net/api/live?access_key=8db4ba3aa504c601dd513777193f4f2b&currencies=%s", tickerCode)
+	text := fmt.Sprintf("%s - %s", tickerCode, name)
+	return dob._leaf(value, text)
 }
 
 func (dob *dropdownOptionsBuilder) _leaf(value string, text string) *dropdownOptionsBuilder {
@@ -340,10 +342,173 @@ func loadOptionsMetadata() (metadata, error) {
 				krakenMarket("XXLM/ZUSD", "XLM/USD"))).
 			includeOptions(ccxtOptions))).
 		option("fiat", "Fiat (CurrencyLayer)", dropdown(optionsBuilder().
-			currencylayer("USD").
-			currencylayer("EUR").
-			currencylayer("GBP").
-			currencylayer("INR"))).
+			currencylayer("AED", "United Arab Emirates Dirham").
+			currencylayer("AFN", "Afghan Afghani").
+			currencylayer("ALL", "Albanian Lek").
+			currencylayer("AMD", "Armenian Dram").
+			currencylayer("ANG", "Netherlands Antillean Guilder").
+			currencylayer("AOA", "Angolan Kwanza").
+			currencylayer("ARS", "Argentine Peso").
+			currencylayer("AUD", "Australian Dollar").
+			currencylayer("AWG", "Aruban Florin").
+			currencylayer("AZN", "Azerbaijani Manat").
+			currencylayer("BAM", "Bosnia-Herzegovina Convertible Mark").
+			currencylayer("BBD", "Barbadian Dollar").
+			currencylayer("BDT", "Bangladeshi Taka").
+			currencylayer("BGN", "Bulgarian Lev").
+			currencylayer("BHD", "Bahraini Dinar").
+			currencylayer("BIF", "Burundian Franc").
+			currencylayer("BMD", "Bermudan Dollar").
+			currencylayer("BND", "Brunei Dollar").
+			currencylayer("BOB", "Bolivian Boliviano").
+			currencylayer("BRL", "Brazilian Real").
+			currencylayer("BSD", "Bahamian Dollar").
+			currencylayer("BTC", "Bitcoin").
+			currencylayer("BTN", "Bhutanese Ngultrum").
+			currencylayer("BWP", "Botswanan Pula").
+			currencylayer("BYR", "Belarusian Ruble").
+			currencylayer("BZD", "Belize Dollar").
+			currencylayer("CAD", "Canadian Dollar").
+			currencylayer("CDF", "Congolese Franc").
+			currencylayer("CHF", "Swiss Franc").
+			currencylayer("CLF", "Chilean Unit of Account (UF)").
+			currencylayer("CLP", "Chilean Peso").
+			currencylayer("CNY", "Chinese Yuan").
+			currencylayer("COP", "Colombian Peso").
+			currencylayer("CRC", "Costa Rican Colón").
+			currencylayer("CUC", "Cuban Convertible Peso").
+			currencylayer("CUP", "Cuban Peso").
+			currencylayer("CVE", "Cape Verdean Escudo").
+			currencylayer("CZK", "Czech Republic Koruna").
+			currencylayer("DJF", "Djiboutian Franc").
+			currencylayer("DKK", "Danish Krone").
+			currencylayer("DOP", "Dominican Peso").
+			currencylayer("DZD", "Algerian Dinar").
+			currencylayer("EGP", "Egyptian Pound").
+			currencylayer("ERN", "Eritrean Nakfa").
+			currencylayer("ETB", "Ethiopian Birr").
+			currencylayer("EUR", "Euro").
+			currencylayer("FJD", "Fijian Dollar").
+			currencylayer("FKP", "Falkland Islands Pound").
+			currencylayer("GBP", "British Pound Sterling").
+			currencylayer("GEL", "Georgian Lari").
+			currencylayer("GGP", "Guernsey Pound").
+			currencylayer("GHS", "Ghanaian Cedi").
+			currencylayer("GIP", "Gibraltar Pound").
+			currencylayer("GMD", "Gambian Dalasi").
+			currencylayer("GNF", "Guinean Franc").
+			currencylayer("GTQ", "Guatemalan Quetzal").
+			currencylayer("GYD", "Guyanaese Dollar").
+			currencylayer("HKD", "Hong Kong Dollar").
+			currencylayer("HNL", "Honduran Lempira").
+			currencylayer("HRK", "Croatian Kuna").
+			currencylayer("HTG", "Haitian Gourde").
+			currencylayer("HUF", "Hungarian Forint").
+			currencylayer("IDR", "Indonesian Rupiah").
+			currencylayer("ILS", "Israeli New Sheqel").
+			currencylayer("IMP", "Manx pound").
+			currencylayer("INR", "Indian Rupee").
+			currencylayer("IQD", "Iraqi Dinar").
+			currencylayer("IRR", "Iranian Rial").
+			currencylayer("ISK", "Icelandic Króna").
+			currencylayer("JEP", "Jersey Pound").
+			currencylayer("JMD", "Jamaican Dollar").
+			currencylayer("JOD", "Jordanian Dinar").
+			currencylayer("JPY", "Japanese Yen").
+			currencylayer("KES", "Kenyan Shilling").
+			currencylayer("KGS", "Kyrgystani Som").
+			currencylayer("KHR", "Cambodian Riel").
+			currencylayer("KMF", "Comorian Franc").
+			currencylayer("KPW", "North Korean Won").
+			currencylayer("KRW", "South Korean Won").
+			currencylayer("KWD", "Kuwaiti Dinar").
+			currencylayer("KYD", "Cayman Islands Dollar").
+			currencylayer("KZT", "Kazakhstani Tenge").
+			currencylayer("LAK", "Laotian Kip").
+			currencylayer("LBP", "Lebanese Pound").
+			currencylayer("LKR", "Sri Lankan Rupee").
+			currencylayer("LRD", "Liberian Dollar").
+			currencylayer("LSL", "Lesotho Loti").
+			currencylayer("LTL", "Lithuanian Litas").
+			currencylayer("LVL", "Latvian Lats").
+			currencylayer("LYD", "Libyan Dinar").
+			currencylayer("MAD", "Moroccan Dirham").
+			currencylayer("MDL", "Moldovan Leu").
+			currencylayer("MGA", "Malagasy Ariary").
+			currencylayer("MKD", "Macedonian Denar").
+			currencylayer("MMK", "Myanma Kyat").
+			currencylayer("MNT", "Mongolian Tugrik").
+			currencylayer("MOP", "Macanese Pataca").
+			currencylayer("MRO", "Mauritanian Ouguiya").
+			currencylayer("MUR", "Mauritian Rupee").
+			currencylayer("MVR", "Maldivian Rufiyaa").
+			currencylayer("MWK", "Malawian Kwacha").
+			currencylayer("MXN", "Mexican Peso").
+			currencylayer("MYR", "Malaysian Ringgit").
+			currencylayer("MZN", "Mozambican Metical").
+			currencylayer("NAD", "Namibian Dollar").
+			currencylayer("NGN", "Nigerian Naira").
+			currencylayer("NIO", "Nicaraguan Córdoba").
+			currencylayer("NOK", "Norwegian Krone").
+			currencylayer("NPR", "Nepalese Rupee").
+			currencylayer("NZD", "New Zealand Dollar").
+			currencylayer("OMR", "Omani Rial").
+			currencylayer("PAB", "Panamanian Balboa").
+			currencylayer("PEN", "Peruvian Nuevo Sol").
+			currencylayer("PGK", "Papua New Guinean Kina").
+			currencylayer("PHP", "Philippine Peso").
+			currencylayer("PKR", "Pakistani Rupee").
+			currencylayer("PLN", "Polish Zloty").
+			currencylayer("PYG", "Paraguayan Guarani").
+			currencylayer("QAR", "Qatari Rial").
+			currencylayer("RON", "Romanian Leu").
+			currencylayer("RSD", "Serbian Dinar").
+			currencylayer("RUB", "Russian Ruble").
+			currencylayer("RWF", "Rwandan Franc").
+			currencylayer("SAR", "Saudi Riyal").
+			currencylayer("SBD", "Solomon Islands Dollar").
+			currencylayer("SCR", "Seychellois Rupee").
+			currencylayer("SDG", "Sudanese Pound").
+			currencylayer("SEK", "Swedish Krona").
+			currencylayer("SGD", "Singapore Dollar").
+			currencylayer("SHP", "Saint Helena Pound").
+			currencylayer("SLL", "Sierra Leonean Leone").
+			currencylayer("SOS", "Somali Shilling").
+			currencylayer("SRD", "Surinamese Dollar").
+			currencylayer("STD", "São Tomé and Príncipe Dobra").
+			currencylayer("SVC", "Salvadoran Colón").
+			currencylayer("SYP", "Syrian Pound").
+			currencylayer("SZL", "Swazi Lilangeni").
+			currencylayer("THB", "Thai Baht").
+			currencylayer("TJS", "Tajikistani Somoni").
+			currencylayer("TMT", "Turkmenistani Manat").
+			currencylayer("TND", "Tunisian Dinar").
+			currencylayer("TOP", "Tongan Paʻanga").
+			currencylayer("TRY", "Turkish Lira").
+			currencylayer("TTD", "Trinidad and Tobago Dollar").
+			currencylayer("TWD", "New Taiwan Dollar").
+			currencylayer("TZS", "Tanzanian Shilling").
+			currencylayer("UAH", "Ukrainian Hryvnia").
+			currencylayer("UGX", "Ugandan Shilling").
+			currencylayer("USD", "United States Dollar").
+			currencylayer("UYU", "Uruguayan Peso").
+			currencylayer("UZS", "Uzbekistan Som").
+			currencylayer("VEF", "Venezuelan Bolívar Fuerte").
+			currencylayer("VND", "Vietnamese Dong").
+			currencylayer("VUV", "Vanuatu Vatu").
+			currencylayer("WST", "Samoan Tala").
+			currencylayer("XAF", "CFA Franc BEAC").
+			currencylayer("XAG", "Silver (troy ounce)").
+			currencylayer("XAU", "Gold (troy ounce)").
+			currencylayer("XCD", "East Caribbean Dollar").
+			currencylayer("XDR", "Special Drawing Rights").
+			currencylayer("XOF", "CFA Franc BCEAO").
+			currencylayer("XPF", "CFP Franc").
+			currencylayer("YER", "Yemeni Rial").
+			currencylayer("ZAR", "South African Rand").
+			currencylayer("ZMK", "Zambian Kwacha (pre-2013)").
+			currencylayer("ZMW", "Zambian Kwacha").
+			currencylayer("ZWL", "Zimbabwean Dollar"))).
 		option("fixed", "Fixed Value", text("1.0"))
 	mdata := dropdown(builder)
 	return mdata, nil
