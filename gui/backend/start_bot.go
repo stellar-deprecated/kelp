da0e5fa5a9472cecec8f2539269c8f8c5427bec3
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2019-09-23 15:45:15 -0700

    do not start bots with-ipc, closes #300

--- gui/backend/start_bot.go
+++ gui/backend/start_bot.go
@@ -38,7 +38,7 @@ func (s *APIServer) startBot(w http.ResponseWriter, r *http.Request) {
 func (s *APIServer) doStartBot(botName string, strategy string, iterations *uint8, maybeFinishCallback func()) error {
 	filenamePair := model2.GetBotFilenames(botName, strategy)
 	logPrefix := model2.GetLogPrefix(botName, strategy)
-	command := fmt.Sprintf("trade -c %s/%s -s %s -f %s/%s -l %s/%s --with-ipc --ui", s.configsDir, filenamePair.Trader, strategy, s.configsDir, filenamePair.Strategy, s.logsDir, logPrefix)
+	command := fmt.Sprintf("trade -c %s/%s -s %s -f %s/%s -l %s/%s --ui", s.configsDir, filenamePair.Trader, strategy, s.configsDir, filenamePair.Strategy, s.logsDir, logPrefix)
 	if iterations != nil {
 		command = fmt.Sprintf("%s --iter %d", command, *iterations)
 	}
