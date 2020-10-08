52a8bd37345d8dba717bd43ea7f981abba22dff8
Debnil Sur <debnil@stellar.org>
2020-10-08 16:16:11 -0700

    Add TODOs.

--- gui/web/src/utils/metricsTracker.js
+++ gui/web/src/utils/metricsTracker.js
@@ -3,12 +3,15 @@ class MetricsTracker {
         this.apiKey = apiKey;
         this.apiUrl = "https://api2.amplitude.com/2/httpapi";
 
-        // TODO: Implement more functions.
+        this._asyncRequests ={};
         this.sendEvent = this.sendEvent.bind(this);
+        // TODO: Implement more functions.
     }
 
     sendEvent(event) {
         var _this = this
+        // TODO: Change namespace.
+        // TODO: Replace `fetch` with a custom routing function.
         this._asyncRequests["fetch"] = fetch(this.apiUrl, {
             method: "POST",
             eventName: event,
@@ -19,6 +22,7 @@ class MetricsTracker {
             }
             delete _this._asyncRequests["fetch"];
 
+            // TODO: log error or ignore successful response.
             return resp.json();
         });
     }
