ca959d762c13cc6516f6489256aa30604a084ce3
Ben Racicot <1815385+BenRacicot@users.noreply.github.com>
2021-03-09 15:16:20 -0500

    dependency variable strategy

--- scripts/install-macos.sh
+++ scripts/install-macos.sh
@@ -2,194 +2,177 @@
 # shellcheck disable=SC2016
 set -e
 
+# Set this machine's dependencies status(es) with the OK flag
+OK=true;
+
+# Dependency test functions begin here
+
 function isNode() {
-    clear
     if node -v; then
-        echo "Node is installed"
-        isYarn
+        OK=true;
     else
-        echo "Node is not installed."
+        OK=false;
     fi
 }
 
 function isYarn() {
     if yarn --version; then
-        echo "Yarn is installed"
-        isGo
+        OK=true;    
     else
-        echo "Yarn is not installed."
+        OK=false;
+    fi
+}
+
+function isGit() {
+    if git --version; then
+        OK=true;
+    else
+        OK=false;
     fi
 }
 
 function isGo() {
     if go version; then
-        echo "Golang is installed at $GOPATH"
+        OK=true;
     else
-        # Go install script based on https://github.com/canha/golang-tools-install-script
-        echo "Golang is not installed. Starting Golang install..."
-
-        VERSION="1.15.7"
-
-        [ -z "$GOROOT" ] && GOROOT="/usr/local/go"
-        [ -z "$GOPATH" ] && GOPATH="$HOME/go"
-
-        OS="$(uname -s)"
-        ARCH="$(uname -m)"
-
-        case $OS in
-            "Linux")
-                case $ARCH in
-                "x86_64")
-                    ARCH=amd64\
-                    ;;
-                "aarch64")
-                    ARCH=arm64
-                    ;;
-                "armv6" | "armv7l")
-                    ARCH=armv6l
-                    ;;
-                "armv8")
-                    ARCH=arm64
-                    ;;
-                .*386.*)
-                    ARCH=386
-                    ;;
-                esac
-                PLATFORM="linux-$ARCH"
-            ;;
-            "Darwin")
-                PLATFORM="darwin-amd64"
-            ;;
-        esac
-
-        if [ -z "$PLATFORM" ]; then
-            echo "Your operating system is not supported by the script."
-            exit 1
-        fi
-
-        if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
-            shell_profile="$HOME/.zshrc"
-        elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
-            shell_profile="$HOME/.bashrc"
-        elif [ -n "$($SHELL -c 'echo $FISH_VERSION')" ]; then
-            shell="fish"
-            if [ -d "$XDG_CONFIG_HOME" ]; then
-                shell_profile="$XDG_CONFIG_HOME/fish/config.fish"
-            else
-                shell_profile="$HOME/.config/fish/config.fish"
-            fi
-        fi
-
-        echo "shell_profile set to $shell_profile"
-
-        if [ -d "$GOROOT" ]; then
-            echo "The Go install directory ($GOROOT) already exists. Exiting."
-            exit 1
-        fi
-
-        PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
-        TEMP_DIRECTORY=$(mktemp -d)
-
-        echo "Downloading $PACKAGE_NAME ..."
-        if hash wget 2>/dev/null; then
-            wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O "$TEMP_DIRECTORY/go.tar.gz"
-        else
-            curl -o "$TEMP_DIRECTORY/go.tar.gz" https://storage.googleapis.com/golang/$PACKAGE_NAME
-        fi
-
-        if [ $? -ne 0 ]; then
-            echo "Download failed! Exiting."
-            exit 1
-        fi
-
-        echo "Extracting File..."
-        mkdir -p "$GOROOT"
-
-        tar -C "$GOROOT" --strip-components=1 -xzf "$TEMP_DIRECTORY/go.tar.gz"
-
-        echo "Configuring shell profile in: $shell_profile"
-        touch "$shell_profile"
-        if [ "$shell" == "fish" ]; then
-            {
-                echo '# GoLang'
-                echo "set GOROOT '${GOROOT}'"
-                echo "set GOPATH '$GOPATH'"
-                echo 'set PATH $GOPATH/bin $GOROOT/bin $PATH'
-            } >> "$shell_profile"
-        else
-            {
-                echo '# GoLang'
-                echo "export GOROOT=${GOROOT}"
-                echo 'export PATH=$GOROOT/bin:$PATH'
-                echo "export GOPATH=$GOPATH"
-                echo 'export PATH=$GOPATH:$PATH'
-            } >> "$shell_profile"
-        fi
-
-        mkdir -p "${GOPATH}/"{src,pkg,bin}
-        echo -e "\nGo $VERSION was installed into $GOROOT.\nMake sure to relogin into your shell or run:"
-        echo -e "\n\tsource $shell_profile\n\nto update your environment variables."
-        echo "Tip: Opening a new terminal window usually just works. :)"
-        rm -f "$TEMP_DIRECTORY/go.tar.gz"
-        exit
-    fi
-
-    cloneIntoDir
-}
-
-# Once we have Go working finish the install processes inside the development directory to avoid errors (Glide)
-function cloneIntoDir() {
-    if go version; then
-        echo "Setting up Kelp folders in the Golang working directory"
-        echo $GOPATH
-        cd $GOPATH
-
-        # pwd
-        sudo mkdir github.com/stellar/kelp
-
-        echo "Cloning Kelp into $GOPATH/src/github.com/stellar/kelp"
-        git clone https://github.com/stellar/kelp.git $GOPATH/src/github.com/stellar/kelp
-
-        cd $GOPATH/src/github.com/stellar/kelp
-
-        isGlide
-    else 
-        echo "Golang not installed, try again."
-        exit
+        OK=false;
     fi
 }
 
 function isGlide() {
     if glide --version; then
-        echo "Glide is installed"
+        OK=true;
     else
-        echo "Installing Glide."
-        if curl --version; then
-            curl https://glide.sh/get | sh
-        elif wget --version; then
-            wget https://glide.sh/get | sh
-        else
-            echo "curl and wget are not available, install glide manually https://github.com/Masterminds/glide"
-            exit
-        fi
+        OK=false;
     fi
-
-    isAstilectron
 }
 
-function isAstilectron() {
-    if go version; then
-        go get -u github.com/asticode/go-astilectron-bundler/... 
-        go install github.com/asticode/go-astilectron-bundler/astilectron-bundler
+# Installation functions begin here
+
+function installGo() {
+    echo 'Installing Golang on your machine.'
+    # Go install script based on https://github.com/canha/golang-tools-install-script
+    VERSION="1.15.7"
+
+    [ -z "$GOROOT" ] && GOROOT="/usr/local/go"
+    [ -z "$GOPATH" ] && GOPATH="$HOME/go"
+
+    OS="$(uname -s)"
+    ARCH="$(uname -m)"
+
+    case $OS in
+        "Linux")
+            case $ARCH in
+            "x86_64")
+                ARCH=amd64\
+                ;;
+            "aarch64")
+                ARCH=arm64
+                ;;
+            "armv6" | "armv7l")
+                ARCH=armv6l
+                ;;
+            "armv8")
+                ARCH=arm64
+                ;;
+            .*386.*)
+                ARCH=386
+                ;;
+            esac
+            PLATFORM="linux-$ARCH"
+        ;;
+        "Darwin")
+            PLATFORM="darwin-amd64"
+        ;;
+    esac
+
+    if [ -z "$PLATFORM" ]; then
+        echo "Your operating system is not supported by the script."
+        exit 1
+    fi
+
+    if [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
+        shell_profile="$HOME/.bashrc"
     else
-        echo "Golang cannot install Astilectron"
+        echo "Kelp requires bash"
+    fi
+
+    if [ -d "$GOROOT" ]; then
+        echo "The Go install directory ($GOROOT) already exists. Exiting."
+        exit 1
+    fi
+
+    PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
+    TEMP_DIRECTORY=$(mktemp -d)
+
+    echo "Downloading $PACKAGE_NAME ..."
+    if hash wget 2>/dev/null; then
+        wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O "$TEMP_DIRECTORY/go.tar.gz"
+    else
+        curl -o "$TEMP_DIRECTORY/go.tar.gz" https://storage.googleapis.com/golang/$PACKAGE_NAME
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Download failed! Exiting."
+        exit 1
+    fi
+
+    echo "Extracting File..."
+
+    sudo chown -R $USER: $HOME # https://github.com/golang/go/issues/27187
+    mkdir -p "$GOROOT"
+
+    tar -C "$GOROOT" --strip-components=1 -xzf "$TEMP_DIRECTORY/go.tar.gz"
+
+    GOROOT=${GOROOT}
+    PATH=$GOROOT/bin:$PATH
+    GOPATH=$GOPATH
+    PATH=$GOPATH:$PATH
+
+    mkdir -p "${GOPATH}/"{src,pkg,bin}
+    echo -e "\nGo $VERSION was installed into $GOROOT.\nMake sure to relogin into your shell or run:"
+    echo -e "\n\ restart script to update your environment variables."
+    echo "Tip: Opening a new terminal window usually just works. :)"
+    rm -f "$TEMP_DIRECTORY/go.tar.gz"
+    exit
+}
+
+# Once we have Golang; finish the install processes inside the development directory to avoid errors (Glide)
+function cloneIntoDir() {
+    echo "Setting up Kelp folders in the Golang working directory"
+    echo $GOPATH
+    cd $GOPATH
+
+    # pwd
+    sudo mkdir github.com/stellar/kelp
+
+    echo "Cloning Kelp into $GOPATH/src/github.com/stellar/kelp"
+    git clone https://github.com/stellar/kelp.git $GOPATH/src/github.com/stellar/kelp
+
+    cd $GOPATH/src/github.com/stellar/kelp
+}
+
+# After Golang install Glide
+function installGlide() {
+    echo "Installing Glide."
+    if curl --version; then
+        curl https://glide.sh/get | sh
+    elif wget --version; then
+        wget https://glide.sh/get | sh
+    else
+        echo "curl and wget are not available, install glide manually https://github.com/Masterminds/glide"
         exit
     fi
-
-    buildAndRun
 }
 
-function buildAndRun() {
+# After Glide install Astilectron
+function installAstilectron() {
+    go get -u github.com/asticode/go-astilectron-bundler/... 
+    go install github.com/asticode/go-astilectron-bundler/astilectron-bundler
+}
+
+# Finally install build Kelp and run it
+function buildKelpAndRun() {
     echo "Building Kelp binaries"
     ./scripts/build.sh
 
@@ -205,6 +188,39 @@ function buildAndRun() {
 }
 
 isNode
+if [ $OK ]; then
+    echo "Node is installed on your machine."
+else
+    echo "Node is not installed on your machine!"
+    exit 1
+fi
 
-echo "Remember, PostgreSQL must be running to store data."
-echo "Remember, Docker and CCXT must be configured for the expanded set of priceFeeds and orderbooks."
+isYarn
+if [ $OK ]; then
+    echo "Yarn is installed on your machine."
+else
+    echo "Yarn is not installed on your machine!"
+    exit 1
+fi
+
+isGit
+if [ $OK ]; then
+    echo "Git is installed on your machine."
+else
+    echo "Git is not installed on your machine!"
+    exit 1
+fi
+
+isGo
+echo $OK
+if $OK; then
+    echo "true for some reason"
+    cloneIntoDir
+    isGlide
+    installAstilectron
+    buildAndRun
+    echo "Remember, PostgreSQL must be running to store data."
+    echo "Remember, Docker and CCXT must be configured for the expanded set of priceFeeds and orderbooks."
+else
+    installGo
+fi
