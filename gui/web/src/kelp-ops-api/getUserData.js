60ea87e024e7853fc6d4830ee4236cbfd9e9892a
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2021-04-09 17:44:19 +0530

    1 - add logic to create and read user_id from localStorage

--- gui/web/src/kelp-ops-api/getUserData.js
+++ gui/web/src/kelp-ops-api/getUserData.js
@@ -1,5 +1,15 @@
+import uuid from "uuid";
+
 export default () => {
+    const userIDKey = 'user_id';
+    
+    let userId = localStorage.getItem(userIDKey);
+    if (userId == null) {
+        userId = uuid.v4();
+        localStorage.setItem(userIDKey, userId);
+    }
+    
     return {
-        ID: "1347129431489",
+        ID: userId,
     };
 };
