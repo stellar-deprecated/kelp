be0dc55ba345a38843c4a70f195ee186320e3db5
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2021-04-09 17:59:14 +0530

    Kelp GUI: allow multiple users on frontend via getUserData() localstorage method (closes #686) (#690)
    * 1 - add logic to create and read user_id from localStorage
    
    * 2 - add uuid dependency to package.json (via npm install)
    
    * 3 - updated yarn lock file after building frontend
    
    * 4 - fix import statement of uuid

--- gui/web/src/kelp-ops-api/getUserData.js
+++ gui/web/src/kelp-ops-api/getUserData.js
@@ -1,5 +1,15 @@
+import { v4 as uuidv4 } from "uuid";
+
 export default () => {
+    const userIDKey = 'user_id';
+    
+    let userId = localStorage.getItem(userIDKey);
+    if (userId == null) {
+        userId = uuidv4();
+        localStorage.setItem(userIDKey, userId);
+    }
+    
     return {
-        ID: "1347129431489",
+        ID: userId,
     };
 };
