5cd4b934e407c99fa8558012102767c47c83d641
clD11 <23483715+clD11@users.noreply.github.com>
2021-07-28 23:56:56 +0100

    feat(277): upgrade dependency management go modules
    update circleci steps for modules

--- scripts/build.sh
+++ scripts/build.sh
@@ -288,6 +288,8 @@ then
 
     # cannot set goarm because not accessible (need to figure out a way)
     echo -n "compiling ... "
+    export GOPROXY=https://goproxy.io,https://proxy.golang.org,https://goproxy.cn
+    go mod downloa
     go build -ldflags "$DYNAMIC_LDFLAGS" -o $OUTFILE
     check_build_result $?
     echo "successful: $OUTFILE"
